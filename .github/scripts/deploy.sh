1747b80ba973b678ed8e59bee471e054bcbbd6ca
cartwatson <cartwatson@gmail.com>
2023-03-07 10:07:59 -0700

    updated deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+# npm run build
 
 # navigate into the build output directory
 cd dist
@@ -13,13 +13,13 @@ cd dist
 echo 'cartwatson.com' > CNAME
 echo 'www.cartwatson.com' > CNAME
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
+# git init
+# git checkout -b main
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
