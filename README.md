843cec211ca28961fdac14271757cf129a7f5fae
cartwatson <cartwatson@gmail.com>
2023-07-01 15:00:51 -0600

    working on navbar, filler content, animated navbar items, added todo's in readme

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ available at [cartwatson.com](http://www.cartwatson.com)
 
 ## Technology
 Created with vanilla html, css, and js  
+
+## TODO - prioritized
+[ ] nav bar  
+[ ] about section  
+[ ] contact section  
+[ ] experience section  
+[ ] single project highlight  
+[ ] project overviews  
+[ ] redo footer  
+[ ] favicon  
+[ ] 
+
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+/* When the user scrolls down, hide the navbar.
+When the user scrolls up, show the navbar */
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
   }
+  prevScrollpos = currentScrollPos;
+}
