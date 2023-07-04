efcaf73efbefd9f2c93cbaa8aa87e81d03931397
cartwatson <cartwatson@gmail.com>
2023-07-04 17:17:47 -0600

    tried updating with animation, async breaks it, work on it more later

--- script.js
+++ script.js
@@ -10,3 +10,70 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+// TODO: FIX: this later fr it's broke as hell
+// // animate title under "Carter Watson" 
+// let titles = ["cyclist", "test1", "software developer"];
+// var title = document.getElementById("title");
+// var titleIndex = 0;
+// let speed = 50;
+
+// function updateTitle() {
+//   if (titleIndex == titles.length) { titleIndex = 0; }
+
+//   let backspaceIndex = title.innerText.length;
+//   let typeIndex = 0;
+
+//   // for (var j = title.innerText.length; j > 1; j--) {
+//   //   title.innerText = title.innerText.substring(0, j);
+//   // }
+
+//   // for (var k = 0; k < titles[titleIndex].length; k++) {
+//   //   title.innerText += titles[titleIndex][k];
+//   // }
+
+//   var typing = true;
+//   console.log("typing");//DBEUG
+//   while (typing) {
+//     typing = backspace();
+//     // if (typing == undefined) { typing = true; }
+//   }
+
+//   typing = true;
+//   while (typing) {
+//     console.log("type");//DEBUG
+//     typing = type();
+//     // if (typing == undefined) { typing = true; }
+//   }
+//   typeIndex = 1000000000;
+
+//   console.log("done typing");//DBEUG
+//   titleIndex++;
+  
+//   function backspace() {
+//     if (backspaceIndex > 1) {
+//       backspaceIndex--;
+//       title.innerText = title.innerText.substring(0, backspaceIndex);
+//       setTimeout(backspace, speed);
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+  
+//   function type() {
+//     console.log("type");//DEBUG
+//     if (typeIndex < titles[titleIndex].length) {
+//       title.innerText += titles[titleIndex][typeIndex];
+//       typeIndex++;
+//       setTimeout(type, speed);
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+
+// // updates to page
+// // updateTitle();
+// setInterval(updateTitle, 6000);
